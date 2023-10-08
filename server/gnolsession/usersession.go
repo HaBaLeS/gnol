04a5cf6deca9c67e8c9c04f8095d8501de62bd58
Falko Lehmann <falko@lehmann-carpzov.de>
2023-10-08 18:04:13 +0200

    Disable Webauthn for outdated dependencys
    Removed Webauthn Support because of broken dependencies with
    security issues

--- server/gnolsession/usersession.go
+++ server/gnolsession/usersession.go
@@ -2,24 +2,23 @@ package gnolsession
 
 import (
 	"github.com/HaBaLeS/gnol/server/storage"
-	"github.com/duo-labs/webauthn/webauthn"
 	"github.com/rs/xid"
 )
 
-//UserSession is bound to gonl cookie and has server side date about the user
+// UserSession is bound to gonl cookie and has server side date about the user
 type UserSession struct {
-	SessionID       string
-	UserName        string
-	UserID          int
-	ComicList       []*storage.Comic
-	SeriesList      *[]storage.Series
-	authenticated   bool
-	D               interface{}
-	WebAuthnSession *webauthn.SessionData //fixme add to registration Session
-	WebAuthnUser    *storage.User         //fixme add to registration Session
+	SessionID     string
+	UserName      string
+	UserID        int
+	ComicList     []*storage.Comic
+	SeriesList    *[]storage.Series
+	authenticated bool
+	D             interface{}
+	//WebAuthnSession *webauthn.SessionData //fixme add to registration Session
+	WebAuthnUser *storage.User //fixme add to registration Session
 }
 
-//NewUserSession creates a gnolsession for Anon and stores it in the Session map
+// NewUserSession creates a gnolsession for Anon and stores it in the Session map
 func NewUserSession() *UserSession {
 	us := &UserSession{
 		SessionID:     xid.New().String(),
@@ -29,12 +28,12 @@ func NewUserSession() *UserSession {
 	return us
 }
 
-//TODO serialize gnolsession, so that we service a restart or can do loadbalancing
+// TODO serialize gnolsession, so that we service a restart or can do loadbalancing
 func (us *UserSession) save() {
 	//Serialize
 }
 
-//Invalidate removes UserSession from cache
+// Invalidate removes UserSession from cache
 func (us *UserSession) Invalidate() {
 	us.authenticated = false
 }
