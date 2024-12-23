03297c4019830947116a3754c8529cde4829d2a3
Falko Lehmann <falko@lehmann-carpzov.de>
2024-12-23 21:55:30 +0100

    remove leech tools

--- cmd/h-leech-tool/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/HaBaLeS/gnol/cmd/h-leech-tool/engine"
-
-func main() {
-	engine.Leech(engine.ILIKECOMIX, "https://ilikecomix.com/western-eroticism/komi-san-cant-fornicate-nudiedoodles/")
-}
